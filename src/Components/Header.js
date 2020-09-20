36392173eced0e88e3dfcc086d4126be52d95ae8
chicori3 <chicori3@naver.com>
2020-09-20 18:17:17 +0900

    Styles Update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = styled.header`
-  color: white;
+  font-weight: bold;
+  color: #333;
   position: fixed;
   top: 0;
   left: 0;
@@ -11,9 +12,9 @@ const Header = styled.header`
   height: 50px;
   display: flex;
   align-items: center;
-  background-color: rgba(20, 20, 20, 0.8);
+  background-color: rgba(243, 243, 243, 1);
   z-index: 10;
-  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.2);
 `;
 
 const List = styled.ul`
@@ -24,6 +25,9 @@ const Item = styled.li`
   width: 80px;
   height: 50px;
   text-align: center;
+  border-bottom: 5px solid
+    ${(props) => (props.current ? "#3498db" : "transparent")};
+  transition: border-bottom 0.5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -33,18 +37,18 @@ const SLink = styled(Link)`
   justify-content: center;
 `;
 
-export default () => (
+export default withRouter(({ location: { pathname } }) => (
   <Header>
     <List>
-      <li>
+      <Item current={pathname === "/"}>
         <SLink to="/">Movies</SLink>
-      </li>
-      <li>
+      </Item>
+      <Item current={pathname === "/tv"}>
         <SLink to="/tv">TV</SLink>
-      </li>
-      <li>
+      </Item>
+      <Item current={pathname === "/search"}>
         <SLink to="/search">Search</SLink>
-      </li>
+      </Item>
     </List>
   </Header>
-);
+));
