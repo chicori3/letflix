e37639d26b9e0d788227c6ff53fd4720387780d2
chicori3 <chicori3@naver.com>
2020-10-06 22:14:21 +0900

    Loader Components

---
+++ src/Components/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  font-size: 20px;
+  margin-top: 15%;
+`;
+
+export default () => <Container>Loading...</Container>;
