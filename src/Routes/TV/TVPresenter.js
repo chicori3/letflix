41a0cbbf8a10a08128114490808248f9e9c3c588
chicori3 <chicori3@naver.com>
2020-10-06 21:45:18 +0900

    TV Presenter

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,11 +1,34 @@
 import React from "react";
 import Proptypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 0 10px;
+`;
 
 const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated Shows">
+          {topRated.map((show) => show.name)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Shows">
+          {popular.map((show) => show.name)}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Airing Today">
+          {airingToday.map((show) => show.name)}
+        </Section>
+      )}
+    </Container>
+  );
 
-TVPresenter.Proptypes = {
+TVPresenter.propTypes = {
   topRated: Proptypes.array,
   popular: Proptypes.array,
   airingToday: Proptypes.array,
