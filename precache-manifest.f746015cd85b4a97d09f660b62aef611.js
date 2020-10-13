b13c881f9ad95fb93f363f8094314ec91cb85f5c
chicori3 <chicori3@naver.com>
2020-10-13 19:41:59 +0900

    Updates

---
+++ precache-manifest.f746015cd85b4a97d09f660b62aef611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41380e21aafa6c6282722870d54344db",
+    "url": "/letflix/index.html"
+  },
+  {
+    "revision": "679c6f7f47236f943364",
+    "url": "/letflix/static/js/2.5965e92c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/letflix/static/js/2.5965e92c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1cf4271eaeb7726ef39",
+    "url": "/letflix/static/js/main.1a4f3317.chunk.js"
+  },
+  {
+    "revision": "178e16782b2c80d439ae",
+    "url": "/letflix/static/js/runtime-main.156c63d2.js"
+  },
+  {
+    "revision": "334a3dafaa8975dca8c17124b5b9d0e6",
+    "url": "/letflix/static/media/noPosterSmall.334a3daf.png"
+  }
+]);
